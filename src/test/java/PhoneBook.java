b99cc3f56cfcd782ed081878df1330af1b8d7c81
Vlad <stormilolz@gmail.com>
2022-07-15 14:03:15 +0300

    HW_5

---
+++ src/test/java/PhoneBook.java
@@ -0,0 +1,68 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.List;
+
+public class PhoneBook {
+    WebDriver webDriver;
+    String url = "https://www.w3schools.com/";
+
+
+    @BeforeTest
+    public void init() {
+        webDriver = new ChromeDriver();
+        webDriver.navigate().to(url);
+        webDriver.findElement(By.xpath("//a[text()='Learn CSS'] [@class='w3-button w3-block tut-button']")).click();
+        webDriver.findElement(By.xpath("//a[text()='CSS Tables']")).click();
+    }
+    @Test
+    public void amountOfTableRows() {
+        List<WebElement> webElementList = webDriver.findElements(By.xpath("//table[@id='customers']//tr"));
+        int numberOfElements = webElementList.size();
+        Assert.assertEquals(numberOfElements, 9);
+    }
+    @Test
+    public void amountOfTableColumns() {
+        List<WebElement> webElementList = webDriver.findElements(By.xpath("//table[@id='customers']//th"));
+        int numberOfElements = webElementList.size();
+        Assert.assertEquals(numberOfElements, 3);
+    }
+    @Test
+    public void contentOfRow3() {
+        List<WebElement> webElementList = webDriver.findElements(By.xpath("//table[@id='customers']//tr[3]/td"));
+        System.out.println("--------Row3---------");
+        for (WebElement element : webElementList) {
+            System.out.println(element.getText());
+        }
+
+    }
+    @Test
+    public void contentOfLastColumn() {
+        List<WebElement> webElementList = webDriver.findElements(By.xpath("//table[@id='customers']//td[last()]"));
+        System.out.println("--------LastColumn---------");
+        for (WebElement element : webElementList) {
+            System.out.println(element.getText());
+        }
+    }
+    @Test
+    public void numberOfRowWithName() {
+        List<WebElement> webElementList = webDriver.findElements(By.xpath("//table[@id='customers']//td[2]"));
+        for (WebElement element : webElementList) {
+            if(element.getText().contains("Philip Cramer")) {
+                System.out.println("-----Philip-----");
+                System.out.println("Row number is: " + (webElementList.indexOf(element) + 1));
+            }
+        }
+
+
+    }
+
+    @AfterTest
+    public void tearDown (){
+        webDriver.quit();
+    }
+}
